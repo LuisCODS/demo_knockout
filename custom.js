aeaa235f7b035e26eec6806b90bdcdecc71176db
Luis Santos <luis.santos@william.coop>
2022-10-16 13:56:37 -0400

    Displaying a total surcharge

--- custom.js
+++ custom.js
@@ -1,3 +1,6 @@
+
+// ========================= EXEMPLE 1 ==============================
+/*
 // Class
 function AppViewModel() {
 
@@ -25,34 +28,66 @@ function AppViewModel() {
 
 // Activates knockout for class
 ko.applyBindings(new AppViewModel());
+*/
 
-// ================================================================
+// =====================  EXEMPLE 2  ==================================
 
 // Class to represent a row in the seat reservations grid
 function SeatReservation(name, initialMeal) {
+    // attributs
     var self = this;
     self.name = name;
     self.meal = ko.observable(initialMeal);
-}
-
-// ===============================================================
+    // methodes
+    self.formattedPrice = ko.computed(function(){
+        // read observable value
+        var price = self.meal().price;
+        return price ? "$" + price.toFixed(2) : "None";
+    });
+}// end class
 
 //class
 function ReservationsViewModel() {
     var self = this;
 
-    // JavaScript object providing meal data
+    // object providing meal data
     self.availableMeals = [
         { mealName: "Standard (sandwich)",      price: 0 },
         { mealName: "Premium (lobster)",        price: 34.95 },
         { mealName: "Ultimate (whole zebra)",   price: 290 }
     ];
-
-    // array collection of SeatReservation instances
+    // observable array collection of SeatReservation instances
     self.seats = ko.observableArray([
         new SeatReservation("Steve", self.availableMeals[0]),
         new SeatReservation("Bert", self.availableMeals[0])
     ]);
+    // push an extra entry into the seats array
+    self.addSeat = function(){
+        self.seats.push( new SeatReservation("", self.availableMeals[0]))
+    };
+    // remove a item from array
+    self.removeSeat = function(seat){
+        self.seats.remove(seat);
+    };
+    // calcul the total to pay
+    self.totalSurcharge = ko.computed(function(){
+       var total = 0;
+       for (let i = 0; i < self.seats().length; i++) {
+         total += self.seats()[i].meal().price;
+       }
+        return total;
+    });
 }
-// Activates knockout for class
+// Activates knockout
 ko.applyBindings(new ReservationsViewModel());
+
+
+// =====================  EXEMPLE 3  ==================================
+/*
+// Class
+function ViewModel(){
+    var self = this;
+    self.monTableau = ko.observableArray([1,2,3,4,5]);
+}
+ko.applyBindings(new ViewModel());
+* */
