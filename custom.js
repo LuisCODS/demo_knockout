0151abb447b484b3d56e7e171232c5767e83d1b5
Luis Santos <luis.santos@william.coop>
2022-10-15 21:08:00 -0400

    code ne marche pas

--- custom.js
+++ custom.js
@@ -1,9 +1,7 @@
-// This is a simple *viewmodel*
-// ========================== class 1 =======================================
-
+// Class
 function AppViewModel() {
 
-    let self = this;
+    var self = this;
     // make properties observable using ko.observable:
     self.firstName = ko.observable("Bert");         // set properties observable
     self.lastName  = ko.observable("Bertington");   // set properties observable
@@ -17,40 +15,44 @@ function AppViewModel() {
     self.capitalizeLastName = function() {
         //  to read or write an observable's value, you call it as a function.
         let currentVal = self.lastName();        // read observable value
-        self.lastName(currentVal.toUpperCase()); // modified observable value
-        currentVal = this.lastName();            // read observable value
-        this.lastName(currentVal.toUpperCase()); // modified observable value
+        self.lastName(currentVal.toUpperCase()); // set observable value
+        currentVal = this.lastName();
+        this.lastName(currentVal.toUpperCase());
     };
+    self.list = ko.observableArray([1,2,3,4,5]);
 
-}
-// Activates knockout to have the Observables
+}// fin class
+
+// Activates knockout for class
 ko.applyBindings(new AppViewModel());
 
-// ========================== class 2 =======================================
+// ================================================================
 
-// JavaScript class constructor that stores a passenger name with a meal selection
+// Class to represent a row in the seat reservations grid
 function SeatReservation(name, initialMeal) {
-    let self = this;
+    var self = this;
     self.name = name;
     self.meal = ko.observable(initialMeal);
 }
 
-// ========================== class 3 =======================================
-// View-model class
+// ===============================================================
+
+//class
 function ReservationsViewModel() {
-    let self = this;
+    var self = this;
 
     // JavaScript object providing meal data
     self.availableMeals = [
-        { mealName: "Standard (sandwich)", price: 0 },
-        { mealName: "Premium (lobster)", price: 34.95 },
-        { mealName: "Ultimate (whole zebra)", price: 290 }
+        { mealName: "Standard (sandwich)",      price: 0 },
+        { mealName: "Premium (lobster)",        price: 34.95 },
+        { mealName: "Ultimate (whole zebra)",   price: 290 }
     ];
 
-    // array holding an initial collection of SeatReservation instances
+    // array collection of SeatReservation instances
     self.seats = ko.observableArray([
-                                        new SeatReservation("Steve", self.availableMeals[0]),
-                                        new SeatReservation("Bert", self.availableMeals[0])
-                                    ]);
+        new SeatReservation("Steve", self.availableMeals[0]),
+        new SeatReservation("Bert", self.availableMeals[0])
+    ]);
 }
+// Activates knockout for class
 ko.applyBindings(new ReservationsViewModel());
