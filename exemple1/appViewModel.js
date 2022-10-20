d02345169ddde13d723e405ee0df94452e7808ad
Luis Santos <luis.santos@william.coop>
2022-10-20 12:53:57 -0400

    2 exemple fixed

--- exemple1/appViewModel.js
+++ exemple1/appViewModel.js
@@ -1,34 +1,29 @@
 
 // ========================= EXEMPLE 1 ==============================
-
-// Class
+// CLASS
 function AppViewModel() {
 
+    // ======================= ATTRIBUTS =======================
     var self = this;
-    // make properties observable using ko.observable:
+    // make properties observable
     self.firstName = ko.observable("Bert");         // set properties observable
     self.lastName  = ko.observable("Bertington");   // set properties observable
-
-    // Defining computed values
-    self.fullName = ko.computed(function() {
-        return self.firstName() + " " + self.lastName();
-    }, self);
+    // Defining pureComputed values
+    self.fullName = ko.pureComputed(function() {
+        return this.firstName() + " " + this.lastName();
+    }, this);
+    // ======================= METHODES =======================
 
     // function to makes the "last name" value turn upper-case.
     self.capitalizeLastName = function() {
         //  to read or write an observable's value, you call it as a function.
         let currentVal = self.lastName();        // read observable value
         self.lastName(currentVal.toUpperCase()); // set observable value
-        currentVal = this.lastName();
-        this.lastName(currentVal.toUpperCase());
+        currentVal = self.lastName();
+        self.lastName(currentVal.toUpperCase());
     };
-    self.list = ko.observableArray([1,2,3,4,5]);
-
 }// fin class
-
-ap = new AppViewModel();
-// Activates knockout for class
-ko.applyBindings(ap);
+ko.applyBindings( new AppViewModel()); // Activates knockout for class
 
 
 
---
+++ exemple2/reservationsViewModel.js
@@ -0,0 +1,58 @@
+
+
+// =====================  EXEMPLE 2  ==================================
+
+// Class to represent a row in the seat reservations grid
+function SeatReservation(name, initialMeal) // end class
+{
+    // attributs
+    let self = this;
+    self.name = name;
+    self.meal = ko.observable(initialMeal);
+    // methodes
+    self.formattedPrice = ko.pureComputed(() => {
+        // read observable value
+        let price = self.meal().price;
+        return price ? "$" + price.toFixed(2) : "None";
+    });
+}
+
+//class
+function ReservationsViewModel()
+{
+    let self = this;
+    // object providing meal data
+    self.availableMeals = [
+        { mealName: "Standard (sandwich)",      price: 0 },
+        { mealName: "Premium (lobster)",        price: 34.95 },
+        { mealName: "Ultimate (whole zebra)",   price: 290 }
+    ];
+    // observable array collection of SeatReservation instances
+    self.seats = ko.observableArray([
+        new SeatReservation("Steve", self.availableMeals[0]),
+        new SeatReservation("Bert", self.availableMeals[0])
+    ]);
+    // push an extra entry into the seats array
+    self.addSeat = function(){
+        self.seats.push( new SeatReservation("", self.availableMeals[0]))
+    };
+    // remove a item from array
+    self.removeSeat = function(seat){
+        self.seats.remove(seat);
+    };
+    // calcul the total to pay
+    self.totalSurcharge = ko.computed(function(){
+        let total = 0;
+        for (let i = 0; i < self.seats().length; i++) {
+         total += self.seats()[i].meal().price;
+       }
+        return total.toFixed(2);
+    });
+}
+// Activates knockout
+ko.applyBindings(new ReservationsViewModel());
+
+
+
+
+
