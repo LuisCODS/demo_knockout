c350893aa7d736337f9bfbe5b94e4c6b7b09276e
Luis Santos <luis.santos@william.coop>
2022-10-28 08:49:33 -0400

    create branch dev

--- exemple3/viewModel3.js
+++ exemple3/viewModel3.js
@@ -1,5 +1,6 @@
 //class
 function ViewModel3(){
+
     var self = this;
     self.list                   = ko.observableArray([]);
     self.input_descriptionItem  = ko.observable();
@@ -9,14 +10,15 @@ function ViewModel3(){
         if (self.input_descriptionItem() == "" ){
             self.alert({dislay: true, message: "Veuillez ajouter un item!", type: "alert-info"})
             return this.list() < 0 ? "alert" : "profitPositive";
-
         }else{
             self.list.push({ description: self.input_descriptionItem(), coche:false })
             self.input_descriptionItem("");//clean fild
             self.alert({dislay: true, message: "Intem ajouté avec succes!", type: "alert-info"})
         }
     };
-}
+
+}// end class
+
 v3 = new ViewModel3();
 // Activates knockout
 ko.applyBindings(v3);
