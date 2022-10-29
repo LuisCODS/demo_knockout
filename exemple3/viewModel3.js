79ae60b16aa0ac06599a6cf89dd04f6526d61b7b
Luis Santos <luis.santos@william.coop>
2022-10-29 19:17:44 -0400

    List with checkbox

--- exemple3/viewModel3.js
+++ exemple3/viewModel3.js
@@ -5,13 +5,13 @@ function ViewModel3(){
 
     self.list               = ko.observableArray([]);
     self.input_description  = ko.observable("");
-    self.alert              = ko.observable( {show: true, msn: "Add intens inside list!", type: "alert alert-secondary"} )
+    self.alert              = ko.observable( {show: true, msn: "Add itens to list!", type: "alert alert-secondary"} )
 
     self.addItem = () => {
         // if no input data
         if (self.input_description() == ""){
             // update objet
-            self.alert({show: true, msn: "Veuillez ajouter un item!", type: "alert alert-danger" })
+            self.alert({show: true, msn: "Warning...no item selected!!", type: "alert alert-danger" })
         }else{
             // add an objet
             self.list.push( {description: self.input_description(), coche: ko.observable(false)} )
@@ -21,12 +21,21 @@ function ViewModel3(){
             self.alert({show: true, msn: "Intem ajouté avec succes!", type: "alert alert-info"})
         }
     };
-    // return total tens checkeds
-    self.itensChecados = ko.pureComputed(function(){
+
+    //  Return a total of checked itens
+    self.totalCheckedItens = ko.pureComputed(function(){
          var count = self.list().filter(function(item){
              return item.coche();
          }).length;
          return count;
+    });
+
+    //  Return a total of unchecked itens
+    self.totalUchekedsItens = ko.pureComputed(function(){
+        var count = self.list().filter(function(item){
+            return !item.coche();
+        }).length;
+        return count;
     });
 
 }// end class
