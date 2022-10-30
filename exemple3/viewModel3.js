e37f66adb0f08373cd2678223d057391bf35a99e
Luis Santos <luis.santos@william.coop>
2022-10-29 21:31:40 -0400

    delete itens from list

--- exemple3/viewModel3.js
+++ exemple3/viewModel3.js
@@ -5,7 +5,8 @@ function ViewModel3(){
 
     self.list               = ko.observableArray([]);
     self.input_description  = ko.observable("");
-    self.alert              = ko.observable( {show: true, msn: "Add itens to list!", type: "alert alert-secondary"} )
+    self.deleteItem         = ko.observable("");
+    self.alert              = ko.observable( {show: true, msn: "Add itens to list!", type: "alert alert-secondary"} );
 
     self.addItem = () => {
         // if no input data
@@ -22,6 +23,16 @@ function ViewModel3(){
         }
     };
 
+    // delete itens from list
+    self.deleteItem = (obj) => {
+        console.log(obj);
+        const index = self.list.indexOf(obj);
+        if (index > -1) { // only splice array when item is found
+            // 2nd parameter means remove one item only
+            self.list.splice(index, 1);
+        }
+    }
+
     //  Return a total of checked itens
     self.totalCheckedItens = ko.pureComputed(function(){
         return count = self.list().filter(function(item){ return item.coche(); }).length;
@@ -37,6 +48,7 @@ function ViewModel3(){
 v3 = new ViewModel3();
 // Activates knockout
 ko.applyBindings(v3);
+
 
 
 
